dbf98f08c629873156f4b829333baf304e27c7a2
angelaa11 <53440596+angelaa11@users.noreply.github.com>
2019-07-29 13:57:30 -0400

    Add files via upload

---
+++ Phython Functions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello")
+
+import plotly
+
+dir(plotly)
+print("I am Angela")
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print(8/9*3)
+print("5*2")
+
+def multiply(a,b):
+    multiply=a*b
+
+def sum(a,b):
+    sum(a+b)
+    print(sum)
+
+def area(a,b,c):
+    area=.5*13*24
+    print(area)
+    
+def triArea(b,h):
+    triArea=.5*b*h
+    print("the area of the triangle with a base of ,'b', and a height of 'h, 'is', triArea)
+    
+triArea(1,2)
